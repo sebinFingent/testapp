7870fd80670cc39da129bfb9324c2f5c9e52edeb
Sebin Jacob <sebin.jacob@fingent.com>
2025-04-30 12:01:54 +0530

    removed date picker

--- src/app/timepicker/page.js
+++ src/app/timepicker/page.js
@@ -4,14 +4,13 @@ import * as React from 'react';
 import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 
 function TimePickerViews() {
     return (
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <DemoContainer components={['TimePicker', 'TimePicker', 'TimePicker']}>
           <DemoItem label={'"hours", "minutes" and "seconds"'}>
-            <TimePicker views={['hours', 'minutes', 'seconds']} />
+           
           </DemoItem>
         </DemoContainer>
       </LocalizationProvider>
